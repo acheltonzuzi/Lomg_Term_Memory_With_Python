a68ed0aefacb97ff990882264832abb05539e017
acheltonzuzi <acheltonzuzi@gmail.com>
2024-05-20 17:03:21 -0700

    a

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def chat(input:Prompt):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="localhost", port=8000)
+    uvicorn.run("main:app", host="localhost", port=8000)
 
 
    
